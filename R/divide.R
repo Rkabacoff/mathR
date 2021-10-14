1889a6b402bb59806010d8bfaa1602e851bd117d
Robert Kabacoff <rkabacoff@wesleyan.edu>
2021-10-14 16:02:57 -0400

    initial commit

---
+++ R/divide.R
@@ -0,0 +1,18 @@
+#'@title Dive one vector by another vector
+#'@description
+#'This function divides one numeric vector by another
+#'numeric vector and returns a numeric vector
+#'
+#'@param x a numeric vector
+#'@param y a numeric vector
+#'
+#'@return a numeric vector
+#'
+#'@export
+#'
+#'@examples
+#'divide(2, 3)
+#'divide(mtcars$mpg, mtcars$hp)
+divide <- function(x, y){
+  x / y
+}
